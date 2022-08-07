3356755cf74e8b6f9d8945d8fecc59a1d3a13500
Max Ullmann <maxullmann@gmail.com>
2022-08-07 14:57:45 +0200

    Initial commit

---
+++ OsuMapDeleter/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace OsuMapDeleter
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSongsFolder = new System.Windows.Forms.Label();
+            this.tbSongsPath = new System.Windows.Forms.TextBox();
+            this.btnOpenFileDialog = new System.Windows.Forms.Button();
+            this.lblModes = new System.Windows.Forms.Label();
+            this.checkBoxStd = new System.Windows.Forms.CheckBox();
+            this.checkBoxTaiko = new System.Windows.Forms.CheckBox();
+            this.checkBoxMania = new System.Windows.Forms.CheckBox();
+            this.checkBoxCtb = new System.Windows.Forms.CheckBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.labelProcessing = new System.Windows.Forms.Label();
+            this.labelProcessed = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblSongsFolder
+            // 
+            this.lblSongsFolder.AutoSize = true;
+            this.lblSongsFolder.Location = new System.Drawing.Point(13, 15);
+            this.lblSongsFolder.Name = "lblSongsFolder";
+            this.lblSongsFolder.Size = new System.Drawing.Size(72, 13);
+            this.lblSongsFolder.TabIndex = 0;
+            this.lblSongsFolder.Text = "Songs Folder:";
+            // 
+            // tbSongsPath
+            // 
+            this.tbSongsPath.Location = new System.Drawing.Point(91, 12);
+            this.tbSongsPath.Name = "tbSongsPath";
+            this.tbSongsPath.Size = new System.Drawing.Size(237, 20);
+            this.tbSongsPath.TabIndex = 1;
+            // 
+            // btnOpenFileDialog
+            // 
+            this.btnOpenFileDialog.Location = new System.Drawing.Point(341, 12);
+            this.btnOpenFileDialog.Name = "btnOpenFileDialog";
+            this.btnOpenFileDialog.Size = new System.Drawing.Size(28, 20);
+            this.btnOpenFileDialog.TabIndex = 2;
+            this.btnOpenFileDialog.Text = "...";
+            this.btnOpenFileDialog.UseVisualStyleBackColor = true;
+            this.btnOpenFileDialog.Click += new System.EventHandler(this.btnOpenFileDialog_Click);
+            // 
+            // lblModes
+            // 
+            this.lblModes.AutoSize = true;
+            this.lblModes.Location = new System.Drawing.Point(13, 45);
+            this.lblModes.Name = "lblModes";
+            this.lblModes.Size = new System.Drawing.Size(42, 13);
+            this.lblModes.TabIndex = 3;
+            this.lblModes.Text = "Modes:";
+            // 
+            // checkBoxStd
+            // 
+            this.checkBoxStd.AutoSize = true;
+            this.checkBoxStd.Location = new System.Drawing.Point(91, 44);
+            this.checkBoxStd.Name = "checkBoxStd";
+            this.checkBoxStd.Size = new System.Drawing.Size(69, 17);
+            this.checkBoxStd.TabIndex = 4;
+            this.checkBoxStd.Text = "Standard";
+            this.checkBoxStd.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxTaiko
+            // 
+            this.checkBoxTaiko.AutoSize = true;
+            this.checkBoxTaiko.Location = new System.Drawing.Point(91, 67);
+            this.checkBoxTaiko.Name = "checkBoxTaiko";
+            this.checkBoxTaiko.Size = new System.Drawing.Size(53, 17);
+            this.checkBoxTaiko.TabIndex = 5;
+            this.checkBoxTaiko.Text = "Taiko";
+            this.checkBoxTaiko.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxMania
+            // 
+            this.checkBoxMania.AutoSize = true;
+            this.checkBoxMania.Location = new System.Drawing.Point(91, 90);
+            this.checkBoxMania.Name = "checkBoxMania";
+            this.checkBoxMania.Size = new System.Drawing.Size(55, 17);
+            this.checkBoxMania.TabIndex = 6;
+            this.checkBoxMania.Text = "Mania";
+            this.checkBoxMania.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxCtb
+            // 
+            this.checkBoxCtb.AutoSize = true;
+            this.checkBoxCtb.Location = new System.Drawing.Point(91, 113);
+            this.checkBoxCtb.Name = "checkBoxCtb";
+            this.checkBoxCtb.Size = new System.Drawing.Size(54, 17);
+            this.checkBoxCtb.TabIndex = 7;
+            this.checkBoxCtb.Text = "Catch";
+            this.checkBoxCtb.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(294, 107);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 9;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(213, 107);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(75, 23);
+            this.btnCheck.TabIndex = 10;
+            this.btnCheck.Text = "Check Files";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // labelProcessing
+            // 
+            this.labelProcessing.AutoSize = true;
+            this.labelProcessing.Location = new System.Drawing.Point(210, 67);
+            this.labelProcessing.Name = "labelProcessing";
+            this.labelProcessing.Size = new System.Drawing.Size(0, 13);
+            this.labelProcessing.TabIndex = 11;
+            // 
+            // labelProcessed
+            // 
+            this.labelProcessed.AutoSize = true;
+            this.labelProcessed.Location = new System.Drawing.Point(210, 86);
+            this.labelProcessed.Name = "labelProcessed";
+            this.labelProcessed.Size = new System.Drawing.Size(0, 13);
+            this.labelProcessed.TabIndex = 12;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(392, 145);
+            this.Controls.Add(this.labelProcessed);
+            this.Controls.Add(this.labelProcessing);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.checkBoxCtb);
+            this.Controls.Add(this.checkBoxMania);
+            this.Controls.Add(this.checkBoxTaiko);
+            this.Controls.Add(this.checkBoxStd);
+            this.Controls.Add(this.lblModes);
+            this.Controls.Add(this.btnOpenFileDialog);
+            this.Controls.Add(this.tbSongsPath);
+            this.Controls.Add(this.lblSongsFolder);
+            this.Name = "MainForm";
+            this.Text = "Osu Diff Deleter";
+            this.Shown += new System.EventHandler(this.MainForm_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSongsFolder;
+        private System.Windows.Forms.TextBox tbSongsPath;
+        private System.Windows.Forms.Button btnOpenFileDialog;
+        private System.Windows.Forms.Label lblModes;
+        private System.Windows.Forms.CheckBox checkBoxStd;
+        private System.Windows.Forms.CheckBox checkBoxTaiko;
+        private System.Windows.Forms.CheckBox checkBoxMania;
+        private System.Windows.Forms.CheckBox checkBoxCtb;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Label labelProcessing;
+        private System.Windows.Forms.Label labelProcessed;
+    }
+}
+
---
+++ OsuMapDeleter/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using Microsoft.WindowsAPICodePack.Taskbar;
+
+namespace OsuMapDeleter
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private bool delStd = false;
+        private bool delTaiko = false;
+        private bool delMania = false;
+        private bool delCtb = false;
+        private List<string> filesToDelete = new List<string>();
+        private int filesProcessed;
+        private int filesFound;
+        private bool done = false;
+
+        private void btnOpenFileDialog_Click(object sender, EventArgs e)
+        {
+            string path;
+            string[] files;
+
+            using (var cofd = new CommonOpenFileDialog())
+            {
+                cofd.InitialDirectory = "C:\\";
+                cofd.IsFolderPicker = true;
+
+                CommonFileDialogResult result = cofd.ShowDialog();
+
+                if (result == CommonFileDialogResult.Ok && !string.IsNullOrWhiteSpace(cofd.FileName))
+                {
+                    tbSongsPath.Text = cofd.FileName;
+                }
+            }
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            filesProcessed = 0;
+            filesFound = 0;
+            done = false;
+            delStd = checkBoxStd.Checked;
+            delTaiko = checkBoxTaiko.Checked;
+            delMania = checkBoxMania.Checked;
+            delCtb = checkBoxCtb.Checked;
+
+            if (!Directory.Exists(tbSongsPath.Text))
+            {
+                MessageBox.Show("Please specify a valid folder");
+                return;
+            }
+
+            var songsPath = tbSongsPath.Text;
+
+            Task.Run(() => ReadFiles(songsPath));
+        }
+
+        private async Task ReadFiles(string songsPath)
+        {
+            List<Task> tasks = new List<Task>();
+
+            foreach (var file in Directory.EnumerateFiles(songsPath, "*.osu", SearchOption.AllDirectories))
+            {
+                tasks.Add(Task.Run(() => CheckFile(file)));
+            }
+
+            await Task.WhenAll(tasks);
+            Console.WriteLine("Done!");
+            done = true;
+        }
+
+        private async Task CheckFile(string file)
+        {
+            using (var reader = new StreamReader(file))
+            {
+                string readText;
+                var counter = 0;
+                filesProcessed++;
+
+                while ((readText = await reader.ReadLineAsync()) != null)
+                {
+                    if (ContainsModeString(readText))
+                    {
+                        filesFound++;
+                        filesToDelete.Add(file);
+                        await Task.Run(() =>
+                        {
+                            Console.WriteLine("Processed: (" + filesFound + "/" + filesProcessed + ")");
+                        });
+                        break;
+                    }
+
+                    if (counter >= 20) break;
+                    counter++;
+                }
+            }
+        }
+
+        private bool ContainsModeString(string line)
+        {
+            if (delStd && line.Contains("Mode: 0")) return true;
+            if (delTaiko && line.Contains("Mode: 1")) return true;
+            if (delCtb && line.Contains("Mode: 2")) return true;
+            if (delMania && line.Contains("Mode: 3")) return true;
+            return false;
+        }
+
+        private void MainForm_Shown(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (!done) return;
+
+            string messageBoxText = "Do you really want to delete?";
+            string caption = "Difficulty Deletion";
+            MessageBoxButtons button = MessageBoxButtons.YesNo;
+            MessageBoxIcon icon = MessageBoxIcon.Warning;
+
+            var result = MessageBox.Show(messageBoxText, caption, button, icon);
+
+            switch (result)
+            {
+                case DialogResult.Yes: Task.Run(DeleteFiles);
+                    break;
+                case DialogResult.No: break;
+            }
+        }
+
+        private async Task DeleteFiles()
+        {
+            List<Task> tasks = new List<Task>();
+
+            foreach (string file in filesToDelete)
+            {
+                Console.WriteLine("Deleted: " + file);
+                tasks.Add(Task.Run(() => File.Delete(file)));
+            }
+
+            await Task.WhenAll(tasks);
+
+            await CleanupDirs(tbSongsPath.Text);
+        }
+
+        private async Task CleanupDirs(string songsPath)
+        {
+            List<Task> tasks = new List<Task>();
+
+            foreach (var dir in Directory.GetDirectories(songsPath, "*", SearchOption.TopDirectoryOnly))
+            {
+                tasks.Add(Task.Run(() => CheckDirForEmptyAndDelete(dir)));
+            }
+        }
+
+        private void CheckDirForEmptyAndDelete(string dir)
+        {
+            if (!Directory.Exists(dir)) return;
+
+            var files = Directory.GetFiles(dir, "*.osu", SearchOption.TopDirectoryOnly);
+
+            if (files.Length == 0)
+            {
+                Console.WriteLine("Deleted: " + dir);
+                Directory.Delete(dir, true);
+            }
+        }
+    }
+}
